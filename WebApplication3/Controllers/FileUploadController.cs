f87c45362e4a6f1dc3e486b6e3016e309f4feb21
ShaoYu <shaoyu.kao@gmail.com>
2017-08-02 00:08:02 +0800

    init

---
+++ WebApplication3/Controllers/FileUploadController.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        // GET: FileUpload
+        [HttpPost]
+        public JsonResult UploadByAjax()
+        {
+            //取得目前 HTTP 要求的 HttpRequestBase 物件
+            foreach (string file in Request.Files)
+            {
+                var fileContent = Request.Files[file];
+                if (fileContent != null && fileContent.ContentLength > 0)
+                {
+                    // 取得的檔案是stream
+                    var stream = fileContent.InputStream;
+                    var fileName = Path.GetFileName(file);
+                    var path = Path.Combine(Server.MapPath("~/Files/"), fileName);
+                    using (var fileStream = System.IO.File.Create(path))
+                    {
+                        stream.CopyTo(fileStream);
+                    }
+                }
+            }
+
+            return Json("Successed");
+        }
+    }
+}
---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
